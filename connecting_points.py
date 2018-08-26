51bf518683b8905ab6e574e35e73ed7ccd673beb
Roy Larson <roy.larson@gmail.com>
2018-08-26 14:46:20 -0500

    testing

--- connecting_points.py
+++ connecting_points.py
@@ -3,10 +3,66 @@ import sys
 import numpy as np
 
 
+class Node:
+    def __init__(self, num):
+        self.num = num
+        self.location = self
+
+    def __repr__(self):
+        return 'Node({})'.format(self.num)
+
+
+def convert_to_np_points(x, y):
+    points = np.array((x, y)).reshape(len(x), 2)
+    return points
+
+
+def calculate_distances(points):
+    # Equation of distance between two points
+    # sqrt(x^2-y^2) = sqrt(x^2 + y^2 - 2*x*y)
+    return np.sqrt((points**2).sum(axis=1)[:, None]
+                   + ((points**2).sum(axis=1)[None, :])
+                   - 2 * points.dot(points.transpose()))
+
+
+def ravel_distances(distances):
+    indices = np.array(np.triu_indices(len(distances), 1)
+                       ).transpose()
+    raveled = [[ind[0], ind[1], distances[ind[0], ind[1]]] for ind in indices]
+    return np.array(raveled)
+
+
+def sort_distances(ravel_dist):
+    sort_dist = ravel_dist[np.lexsort(([1, 0, 1]*ravel_dist).T)]
+    return sort_dist
+
+
+def is_valid_connection(node):
+    if node.location == node:
+        return True
+    else:
+        return False
+
+
 def minimum_distance(x, y):
-    result = 0.
+    nodes = [Node(i) for i in range(len(x))]
+    np_points = convert_to_np_points(x, y)
+    distances = sort_distances(ravel_distances(calculate_distances(np_points)))
+    used = set()
+    total_distance = 0
+    i = 0
+    while len(used) < len(nodes):
+        froms, tos, dist = distances[i]
+        froms = int(froms)
+        tos = int(tos)
+        if is_valid_connection(nodes[tos]):
+            nodes[tos].location = nodes[froms]
+            used.add(nodes[tos])
+            total_distance += dist
+        i += 1
+
     # write your code here
-    return result
+    return total_distance
 
 
 if __name__ == '__main__':
--- test_connecting_points.py
+++ test_connecting_points.py
@@ -1,28 +1,86 @@
 import connecting_points as cp
 import numpy as np
+import pytest
 
 
-def test_einsum():
-    a = np.arange(25).reshape(5, 5)
-#    print(f'a:\n{a}')
+def test_convert_to_points():
+    x = [0, 1, 2, 3, 4, 5]
+    y = [6, 7, 8, 9, 10, 11]
+    points = cp.convert_to_np_points(x, y)
+    ans = np.arange(12).reshape(6, 2)
+    assert points.all() == ans.all()
 
-    ans = np.einsum('ii', a)
-#    print(f'Answer:\n{ans}')
 
-    b = np.arange(5)
-    ans = np.einsum('ij,j', a, b)
-#    print(f'A:\n{a}\nB:\n{b}\nAns:\n{ans}')
+def test_distance():
+    x = [0, 1, 2]
+    y = [0, 1, 2]
+    points = cp.convert_to_np_points(x, y)
+    ans = np.array([[0, np.sqrt(2), np.sqrt(4)],
+                    [np.sqrt(2), 0, np.sqrt(2)],
+                    [np.sqrt(4), np.sqrt(2), 0]])
 
-    c = np.arange(6).reshape(2, 3)
-    ans = np.einsum('ji', c)
-#    print(f'C:\n{c}\nAns:\n{ans}')
+    assert cp.calculate_distances(points).all() == ans.all()
 
-    a = np.arange(10).reshape(5, 2)
-    b = np.transpose(a)
-    #b = np.arange(25).reshape(5, 5)
-    ans = np.einsum('ij,ji->ij', a, b)
-    print(f'A:\n{a}\nB:\n{b}\nAns:\n{ans}')
+
+def test_ravel_froms_tos_distances():
+    x = [0, 1, 2]
+    y = [0, 1, 2]
+    points = cp.convert_to_np_points(x, y)
+    dist = cp.calculate_distances(points)
+
+    answer = [[0, 1, np.sqrt(2)],
+              [0, 2, np.sqrt(4)],
+              [1, 2, np.sqrt(2)]]
+
+    answer = np.array(answer)
+    assert cp.ravel_distances(dist).all() == answer.all()
+
+
+def test_sort_points():
+    x = [0, 1, 2]
+    y = [0, 1, 2]
+    points = cp.convert_to_np_points(x, y)
+    dist = cp.calculate_distances(points)
+
+    answer_1 = cp.sort_distances(cp.ravel_distances(dist))
+
+    x = [1, 0, 2]
+    y = [1, 0, 2]
+    points = cp.convert_to_np_points(x, y)
+    dist = cp.calculate_distances(points)
+
+    answer_2 = cp.sort_distances(cp.ravel_distances(dist))
+    print(answer_1)
+    print(answer_2)
+    assert answer_1.all() == answer_2.all()
+
+
+def test_node():
+    node = cp.Node(0)
+    assert node
+    assert node.num == 0
+    assert node.location == node
+
+
+def test_is_valid_connection():
+
+    node_1 = cp.Node(0)
+    assert cp.is_valid_connection(node_1)
+
+    node_2 = cp.Node(1)
+    node_2.location = node_1
+    assert not cp.is_valid_connection(node_2)
+
+
+def test_minumum_distance():
+    test_1_x = [0, 0, 1, 1]
+    test_1_y = [0, 1, 0, 1]
+    assert cp.minimum_distance(
+        test_1_x, test_1_y) == pytest.approx(3.0, rel=1e-7)
 
 
 if __name__ == '__main__':
-    test_einsum()
+    #    test_distance()
+    #    test_convert_to_points()
+    #    test_sort_points()
+    test_minumum_distance()
